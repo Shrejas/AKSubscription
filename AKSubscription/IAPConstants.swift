32a0fd57c744f664031682d94f22df3c5e92394b
Shrejash <idevshrejas@gmail.com>
2025-05-01 17:08:54 +0530

    - updated the iap constants

--- AKSubscription/IAPConstants.swift
+++ AKSubscription/IAPConstants.swift
@@ -1,3 +1,4 @@
+
 //
 //  IAPConstants.swift
 //  SimpleSubscriptipns
@@ -24,7 +25,7 @@ public struct IAPConstants {
     public static var nonConsumable: [String] = []
     
     /// Set available products dynamically from an external source
-    /// 
+    ///
     @MainActor
     public static func configure(
         autoRenewable: [String],
